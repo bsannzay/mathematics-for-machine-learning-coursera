0a6778bd496125a6d95a0f2590cf443613273d54
Ezequiel França dos Santos <ezefranca@users.noreply.github.com>
2018-03-13 16:42:20 -0300

    update cramer's

--- linear_algebra/cramer.py
+++ linear_algebra/cramer.py
@@ -1,40 +1,22 @@
+def start():
+    print('Enter number of unknowns ex: (x, y = 2) (x, y, z = 3)')
+    n = int(input())
+    return readMatrix(n)
 
-def inputMatrix():
-    nn_matrix = raw_input().split()
-    total_cells =  len(nn_matrix)
-    row_cells = int(total_cells**0.5)
-    matrix = [nn_matrix[i:i+row_cells] for i in xrange(0, total_cells, row_cells)]
-    return matrixToInt(matrix)
+def readMatrix(n):
+    print('Sequentially enter the elements of matrix n*(n+1)')
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(n + 1):
+            matrix[i].append(float(input()))
+    return cramer(matrix)
 
-def matrixToInt(matrix):
-    m3 = matrix
-    h, w = calculate_dimensions(matrix)
-    for i in range(0,h):
-        for j in range(0,w):
-            m3[i][j] = int(matrix[i][j])
-    return m3
 
-def calculate_dimensions(matrix):
-    height = len(matrix)
-    width = 1
-    for item in matrix:
-        width = len(item)
-        break
-    return height, width
-
-def calculate_Dx(matrix):
-    return
-
-def determinat(matrix):
-    heigth, width = calculate_dimensions(matrix)
-  
-    if heigth != width:
-        print('-------')
-        print(matrix)
-        print(heigth)
-        print(width)
-        print('-------')
-        return -666
+def det(matrix, mul = 1):
+    width = len(matrix)
+    if width == 1:
+        return mul * matrix[0][0]
     else:
         sign = -1
         total = 0
@@ -47,7 +29,33 @@ def determinat(matrix):
                         buff.append(matrix[j][k])
                 m.append(buff)
             sign *= -1
-            total += 1 * determinat(m, sign * matrix[0][i])
+            total += mul * det(m, sign * matrix[0][i])
         return total
 
- 
+
+def cramer(matrix):
+    n = len(matrix)
+    mainMat = []
+    solutions = []
+    for i in range(n):
+        mainMat.append([])
+        for j in range(n):
+            mainMat[i].append(matrix[i][j])
+    mainDet = det(mainMat)
+
+    if mainDet != 0:
+        for r in range(n):
+            nowMat = []
+            for i in range(n):
+                nowMat.append([])
+                for j in range(n):
+                    if j == r:
+                        nowMat[i].append(matrix[i][n])
+                    else:
+                        nowMat[i].append(matrix[i][j])
+            print('x', r+1, '=', det(nowMat) / mainDet)
+            solutions.append(det(nowMat) / mainDet)
+    return solutions
+
+
+start()
